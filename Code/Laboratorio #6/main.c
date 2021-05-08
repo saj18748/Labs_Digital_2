d426c33991fd1da6745e7efb3f6563abc41fd571
saj18748 <68131346+saj18748@users.noreply.github.com>
2021-05-08 17:35:59 -0600

    Final lab 6

--- Code/Laboratorio #6/main.c
+++ Code/Laboratorio #6/main.c
@@ -1,3 +1,6 @@
+//Universidad del valle de Guatemala
+//Laboratorio # 6
+//Yefry Sajquiy - 18748
 
 //*****************************************************************************
 
@@ -9,10 +12,8 @@
 #include "driverlib/sysctl.h"
 
 //*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
+
+
 #ifdef DEBUG
 void
 __error__(char *pcFilename, uint32_t ui32Line)
@@ -29,11 +30,9 @@ int main(void){
     SysCtlClockSet(SYSCTL_SYSDIV_64|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
     volatile uint32_t ui32Loop;
-    int i, FLAG;
+    int i, FLAG;   // Para anti rebote
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
     //
@@ -43,28 +42,26 @@ int main(void){
     {
     }
 
-    //
-    // Enable the GPIO pin for the LED (PG2).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);     //  Pines INPUT
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);  // PULL UP
 
     //
     // Loop forever.
     //
-    while(1){
+    while(1){   // Inicio de anti rebote
 
         if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) == 0){
             FLAG = 1;
         }
         else if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) == 16) {
             if(FLAG == 1){
+
+                // inicio de secuencia de semaforo
                 FLAG = 0;
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_3);
                 SysCtlDelay(2000000);
 
-                for (i = 0; i<5; i++){
+                for (i = 0; i<5; i++){  //
                     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_3);
                     SysCtlDelay(200000);
                     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
