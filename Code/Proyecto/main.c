21a650ada9f7dfcf85794fe8292ec5e2b4d2d81d
saj18748 <68131346+saj18748@users.noreply.github.com>
2021-05-20 10:23:50 -0600

    P4

---
+++ Code/Proyecto/main.c
@@ -0,0 +1,188 @@
+//************************************************************************************************
+// Librerías
+//************************************************************************************************
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <WebServer.h>
+//************************************************************************************************
+// Variables globales
+//************************************************************************************************
+// SSID & Password
+
+
+WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
+
+uint8_t LED1pin = 2;
+bool LED1status = LOW;
+
+//************************************************************************************************
+// Configuración
+//************************************************************************************************
+void setup() {
+  Serial.begin(115200);
+  if (!SPIFFS.begin()) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  Serial.println("Try Connecting to ");
+  Serial.println(ssid);
+
+  pinMode(LED1pin, OUTPUT);
+
+  // Connect to your wi-fi modem
+  WiFi.begin(ssid, password);
+
+  // Check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+
+  server.on("/", handle_OnConnect); // Directamente desde e.g. 192.168.0.8
+  server.on("/led1on", handle_led1on);
+  server.on("/led1off", handle_led1off);
+  server.onNotFound([]() {                  // If the client requests any URI
+    if (!HandleFileRead(server.uri()))      // send it if it exists
+      handleNotFound();             // otherwise, respond with a 404 (Not Found) error
+  });
+  //server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(100);
+}
+//************************************************************************************************
+// loop principal
+//************************************************************************************************
+void loop() {
+  server.handleClient();
+  if (LED1status)
+  {
+    digitalWrite(LED1pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED1pin, LOW);
+  }
+}
+//************************************************************************************************
+// Handler de Inicio página
+//************************************************************************************************
+void handle_OnConnect() {
+  LED1status = LOW;
+  Serial.println("GPIO2 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status));
+}
+//************************************************************************************************
+// Handler de led1on
+//************************************************************************************************
+void handle_led1on() {
+  LED1status = HIGH;
+  Serial.println("GPIO2 Status: ON");
+  server.send(200, "text/html", SendHTML(LED1status));
+}
+//************************************************************************************************
+// Handler de led1off
+//************************************************************************************************
+void handle_led1off() {
+  LED1status = LOW;
+  Serial.println("GPIO2 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status));
+}
+//************************************************************************************************
+// Procesador de HTML
+//************************************************************************************************
+String SendHTML(uint8_t led1stat) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>LED Control</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr += ".button-on {background-color: #3498db;}\n";
+  ptr += ".button-on:active {background-color: #2980b9;}\n";
+  ptr += ".button-off {background-color: #34495e;}\n";
+  ptr += ".button-off:active {background-color: #2c3e50;}\n";
+  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<h1>ESP32 Web Server &#128664</h1>\n";
+  ptr += "<h3>Ejemplo de Web Server</h3>\n";
+
+  if (led1stat)
+  {
+    ptr += "<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
+  }
+  else
+  {
+    ptr += "<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
+  }
+
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
+}
+//************************************************************************************************
+// Handler de not found
+//************************************************************************************************
+void handleNotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+//************************************************************************************************
+// Obtener el tipo de contenido del archivo
+//************************************************************************************************
+String GetContentType(String filename)
+{
+  if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+//************************************************************************************************
+// Enviar al servidor el archivo desde SPIFFS
+//************************************************************************************************
+void ServeFile(String path)
+{
+  File file = SPIFFS.open(path, "r");
+  size_t sent = server.streamFile(file, GetContentType(path));
+  file.close();
+}
+//************************************************************************************************
+// Enviar al servidor el archivo desde SPIFFS
+//************************************************************************************************
+void ServeFile(String path, String contentType)
+{
+  File file = SPIFFS.open(path, "r");
+  size_t sent = server.streamFile(file, contentType);
+  file.close();
+}
+//************************************************************************************************
+// Handler de not found
+//************************************************************************************************
+bool HandleFileRead(String path)
+{
+  if (path.endsWith("/")) path += "index.html";
+  Serial.println("handleFileRead: " + path);
+
+  if (SPIFFS.exists(path))
+  {
+    ServeFile(path);
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false;
+}
